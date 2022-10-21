8c183aacde1426a0638e6d929b2c5425384868d4
Alan Cruz <alanlucascruz@gmail.com>
2022-10-21 15:11:21 -0300

    Botões adicionados: Repeat e Volume

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,59 @@
 const btnPlay = document.querySelector("#btn-play");
 const btnPlayIcon = document.querySelector("#btn-play-icon");
+const btnRepeat = document.querySelector("#btn-repeat");
 const btnPrev = document.querySelector("#btn-prev");
 const btnNext = document.querySelector("#btn-next");
-const musicName = document.querySelector("#music-name");
-const musicAuthor = document.querySelector("#music-author");
+const btnVolume = document.querySelector("#btn-volume");
+const btnVolumeIcon = document.querySelector("#btn-volume i");
+const playerVolume = document.querySelector("#player-volume");
+const songName = document.querySelector("#song-name");
+const songAuthor = document.querySelector("#song-author");
 const playerCurrentTime = document.querySelector("#player-current-time");
 const playerDuration = document.querySelector("#player-duration");
 const playerProgress = document.querySelector("#player-progress");
 const audioPlayer = document.querySelector("#audio-player");
 
-let currentMusic = 0;
+let currentSong = 0;
+let repeatSong = false;
 
-const musics = [
+const songs = [
   {
     name: "Stomping Rock",
     author: "Alex Grohl",
-    path: "./assets/musics/Alex Grohl - Stomping Rock.mp3",
+    path: "./assets/songs/Alex Grohl - Stomping Rock.mp3",
   },
   {
     name: "Jazzy Abstract Beat",
     author: "Coma Media",
-    path: "./assets/musics/Coma Media - Jazzy Abstract Beat.mp3",
+    path: "./assets/songs/Coma Media - Jazzy Abstract Beat.mp3",
   },
   {
     name: "Sexy Fashion Beats",
     author: "Coma Media",
-    path: "./assets/musics/Coma Media - Sexy Fashion Beats.mp3",
+    path: "./assets/songs/Coma Media - Sexy Fashion Beats.mp3",
   },
   {
     name: "Best Time",
     author: "FASSounds",
-    path: "./assets/musics/FASSounds - Best Time.mp3",
+    path: "./assets/songs/FASSounds - Best Time.mp3",
   },
   {
     name: "Guitar Electro Sport",
     author: "Gvidon",
-    path: "./assets/musics/Gvidon - Guitar Electro Sport Trailer.mp3",
+    path: "./assets/songs/Gvidon - Guitar Electro Sport Trailer.mp3",
   },
 ];
 
-btnPlay.addEventListener("click", () => togglePlayMusic());
-btnPrev.addEventListener("click", () => changeMusic(false));
-btnNext.addEventListener("click", () => changeMusic());
+btnPlay.addEventListener("click", () => togglePlaySong());
+btnPrev.addEventListener("click", () => changeSong(false));
+btnNext.addEventListener("click", () => changeSong());
+btnRepeat.addEventListener("click", () => toggleRepeatSong());
+playerVolume.addEventListener("input", () => changeVolume());
+playerProgress.addEventListener("input", () => changeTime());
 audioPlayer.addEventListener("timeupdate", () => timeUpdate());
+audioPlayer.addEventListener("ended", () => ended());
 
-const togglePlayMusic = () => {
+const togglePlaySong = () => {
   if (audioPlayer.paused) {
     audioPlayer.play();
     btnPlayIcon.classList.replace("bi-play-fill", "bi-pause-fill");
@@ -54,25 +63,31 @@ const togglePlayMusic = () => {
   }
 };
 
-const changeMusic = (next = true) => {
-  if (next && currentMusic < musics.length - 1) {
-    currentMusic++;
-  } else if (!next && currentMusic > 0) {
-    currentMusic--;
+const changeSong = (next = true) => {
+  if (next && currentSong < songs.length - 1) {
+    currentSong++;
+  } else if (!next && currentSong > 0) {
+    currentSong--;
   } else {
     return;
   }
 
   updatePlayer();
-  togglePlayMusic();
+  togglePlaySong();
 };
 
 const updatePlayer = () => {
-  const music = musics[currentMusic];
+  const song = songs[currentSong];
 
-  musicName.innerHTML = music.name;
-  musicAuthor.innerHTML = music.author;
-  audioPlayer.src = music.path;
+  songName.innerHTML = song.name;
+  songAuthor.innerHTML = song.author;
+  audioPlayer.src = song.path;
+  playerProgress.value = audioPlayer.currentTime;
+};
+
+const toggleRepeatSong = () => {
+  repeatSong = !repeatSong;
+  btnRepeat.classList.toggle("btn-activated");
 };
 
 const timeUpdate = () => {
@@ -86,8 +101,28 @@ const timeUpdate = () => {
   playerProgress.value = currentTime;
 };
 
+const changeVolume = () => {
+  const { value } = playerVolume;
+
+  audioPlayer.volume = value;
+
+  if (value === 0) {
+    btnVolumeIcon.classList.replace("bi-volume-up-fill", "bi-volume-mute-fill");
+  } else {
+    btnVolumeIcon.classList.replace("bi-volume-mute-fill", "bi-volume-up-fill");
+  }
+};
+
+const changeTime = () => {
+  audioPlayer.currentTime = playerProgress.value;
+};
+
 const formatSecondsToMinutes = (seconds) => {
   return new Date(seconds * 1000).toISOString().slice(14, 19);
+};
+
+const ended = () => {
+  repeatSong ? togglePlaySong() : changeSong(true);
 };
 
 window.onload = () => {
